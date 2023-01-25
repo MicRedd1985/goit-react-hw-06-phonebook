39658d12529a865042da3a5071ebee6322c97e7f
MicRedd1985 <micredd1985@gmail.com>
2023-01-25 15:21:21 +0200

    fix
    fix filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,14 @@ import React from 'react';
 import './App.css';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
-import Filter from './Filter/Filter';
-import { useDispatch, useSelector } from 'react-redux';
-import { setFilter } from "../redux/ToolkitSlice";
 
 export default function App() {
-  const dispatch = useDispatch();
-  const filter = useSelector(state => state.filter);
-
-  const handleChange = evt => {
-    dispatch(setFilter(evt.currentTarget.value));
-  };
   return (
     <div className="App">
       <h1>Phonebook</h1>
       <ContactForm />
 
       <h2>Contacts</h2>
-      <Filter handleChange={handleChange} filter={filter} />
       <ContactList />
     </div>
   );
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Label } from './Filter.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from './redux/ToolkitSlice';
 
-const Filter = ({ handleChange, filter }) => (
-  <Label>
-    Find contacts by Name
-    <input onChange={handleChange} type="text" name="filter" value={filter} />
-  </Label>
-);
+function Filter() {
+  const dispatch = useDispatch();
+  const filter = useSelector(state => state.filter);
+
+  const handleChange = evt => dispatch(setFilter(evt.currentTarget.value));
+
+  return (
+    <Label>
+      Find contacts by Name
+      <input onChange={handleChange} type="text" name="filter" value={filter} />
+    </Label>
+  );
+}
 
 export default Filter;
-
-Filter.propTypes = {
-  handleChange: PropTypes.func.isRequired,
-  filter: PropTypes.string.isRequired,
-};
