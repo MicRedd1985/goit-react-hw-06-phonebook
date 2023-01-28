f76db99749090ff4d30ed1f89ccb2b146f2f8812
MicRedd1985 <micredd1985@gmail.com>
2023-01-28 14:05:40 +0200

    fix

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-import { Label } from './Filter.styled';
-import { useDispatch, useSelector } from 'react-redux';
-import { setFilter } from './redux/ToolkitSlice';
+import React from "react";
+import PropTypes from "prop-types";
+import { Label } from "./Filter.styled";
 
-function Filter() {
-  const dispatch = useDispatch();
-  const filter = useSelector(state => state.filter);
-
-  const handleChange = evt => dispatch(setFilter(evt.currentTarget.value));
-
-  return (
-    <Label>
-      Find contacts by Name
-      <input onChange={handleChange} type="text" name="filter" value={filter} />
-    </Label>
-  );
-}
+const Filter = ({ handleChange, filter }) => (
+  <Label>
+    Find contacts by Name
+    <input onChange={handleChange} type="text" name="filter" value={filter} />
+  </Label>
+);
 
 export default Filter;
+
+Filter.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
+};
