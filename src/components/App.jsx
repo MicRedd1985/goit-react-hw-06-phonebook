f03a98891a23146af78f306070740ec5f631e2a7
MicRedd1985 <micredd1985@gmail.com>
2023-01-28 15:31:03 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,29 +2,16 @@ import React from 'react';
 import './App.css';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
-import { useDispatch, useSelector } from 'react-redux';
 import Filter from "./Filter/Filter";
-import { setFilter } from '../redux/ToolkitSlice';
 
 export default function App() {
-
-  const dispatch = useDispatch();
-  const filter = useSelector(state => state.filter);
-
-
-  const handleChange = evt => {
-    dispatch(setFilter(evt.currentTarget.value));
-  };
-
-    return (
-      
-      <div className="App">
-        <h1>Phonebook</h1>
-        <ContactForm />
-
-        <h2>Contacts</h2>
-        <Filter handleChange={handleChange} filter={filter} />
-        <ContactList />
-      </div>
-    );
+  return (
+    <div className="App">
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter/>
+      <ContactList />
+    </div>
+  );
 }
